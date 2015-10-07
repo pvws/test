2ec0d18c9f40bf4f1dbc3c6b70ba20c297efde24
Michell Fehr <michell.fehr@web.de>
2015-10-07 20:11:41 +0200

    REST Article Sceleton

--- SWDataExImporter/SWDataExImporter/de/pvws/swtools/util/REST/PullArticleFromSW.java
+++ SWDataExImporter/SWDataExImporter/de/pvws/swtools/util/REST/PullArticleFromSW.java
@@ -3,6 +3,12 @@
  */
 package de.pvws.swtools.util.REST;
 
+import de.pvws.swtools.swDataStructure.*;
+
+import java.util.*;
+
+import javax.ws.rs.core.*;
+import javax.ws.rs.client.*;
 
 /**
  * 
@@ -11,4 +17,29 @@ package de.pvws.swtools.util.REST;
  */
 public class PullArticleFromSW {
 
+	public PullArticleFromSW (LinkedList<SWArticle> llSwa) {
+		
+	}
+	
+	public static void doPull () {
+		Client client;
+		WebTarget wtHost;
+		Invocation.Builder invB;
+		Response resp;
+		
+		client = ClientBuilder.newClient();
+		client.property("user", "api");
+		client.property("password","password");
+		
+		wtHost = client.target("http://shopware-dev/api/article");
+		invB =  wtHost.request();
+		invB.accept("application/json");
+		invB.acceptEncoding("charset=utf-8");
+		// Content-Type: application/json; charset=utf-8
+		
+		// Post --> create
+		// Put --> update
+		resp = wtHost.request().get();
+		
+	}
 }
--- SWDataExImporter/SWDataExImporter/de/pvws/swtools/util/REST/PushArticleToSW.java
+++ SWDataExImporter/SWDataExImporter/de/pvws/swtools/util/REST/PushArticleToSW.java
@@ -3,6 +3,12 @@
  */
 package de.pvws.swtools.util.REST;
 
+import de.pvws.swtools.swDataStructure.*;
+
+import java.util.*;
+
+import javax.ws.rs.core.*;
+import javax.ws.rs.client.*;
 
 /**
  * 
@@ -10,5 +16,28 @@ package de.pvws.swtools.util.REST;
  *
  */
 public class PushArticleToSW {
+	
+	public PushArticleToSW () {
+		
+	}
+	
+	public static void doPush () {
+		Client client;
+		WebTarget wtHost;
+		Invocation.Builder invB;
+		Response resp;
+		
+		client = ClientBuilder.newClient();
+		client.property("user", "api");
+		client.property("password","password");
+		
+		wtHost = client.target("http://shopware-dev/api/article");
+		invB =  wtHost.request();
+		invB.accept(MediaType.APPLICATION_JSON_TYPE);
+		invB.acceptEncoding("charset=utf-8");
+		// Content-Type: application/json; charset=utf-8
+		
+		resp = wtHost.request().get();
+	}
 
 }
